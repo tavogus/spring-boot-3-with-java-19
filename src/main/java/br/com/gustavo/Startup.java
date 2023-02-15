79957dad8732c24af8b2e88b5ccc35283b58de57
gustavo <gustavobesantanna@gmail.com>
2023-02-14 22:23:38 -0300

    finalizando endpoint de autenticação

--- src/main/java/br/com/gustavo/Startup.java
+++ src/main/java/br/com/gustavo/Startup.java
@@ -2,6 +2,12 @@ package br.com.gustavo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class Startup {
@@ -9,9 +15,9 @@ public class Startup {
 	public static void main(String[] args) {
 		SpringApplication.run(Startup.class, args);
 
-		/**
+
 		 Map<String, PasswordEncoder> encoders = new HashMap<>();
-		 Pbkdf2PasswordEncoder encoder = new Pbkdf2PasswordEncoder("", 8, 185000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+		 Pbkdf2PasswordEncoder encoder = new Pbkdf2PasswordEncoder("", 8, 185000, Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
 		 encoders.put("pbkdf2", encoder);
 		 DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
 		 passwordEncoder.setDefaultPasswordEncoderForMatches(encoder);
@@ -19,7 +25,7 @@ public class Startup {
 		 String result = passwordEncoder.encode("admin123");
 		 String result2 = passwordEncoder.encode("admin234");
 		 System.out.println("My hash " + result);
-		 */
+
 	}
 
 }
--- src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
+++ src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
@@ -1,3 +1,3 @@
 ﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
-	('gustavo', 'Gustavo Becker', '1e3cdeeaaaeeda173ff6d002e7cb5e3f91ebc354dcff52156c9eaba1793a3a5e5bee306c11099e22', b'1', b'1', b'1', b'1'),
+	('gustavo', 'Gustavo Becker', 'd9dd76e9cc0c5d99a763b93a657d983496858208210b79280dd09d62d4cd9fe9adea67e5af5f4e06', b'1', b'1', b'1', b'1'),
 	('mariana', 'Mariana ramos', '362ad02420268beeb22d3a1f0d92749df461d7f4b74c9433d7415bdeef1b2902f4eb1edaecb37cb3', b'1', b'1', b'1', b'1');
